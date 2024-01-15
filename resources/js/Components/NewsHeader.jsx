4a7f16ef2b8c86fbcea5545a56c7624eddf49108
Iqbal Al Hafidzu Rahman <iqbal.al21@students.unila.ac.id>
2024-01-15 13:11:26 +0700

    update

--- resources/js/Components/NewsHeader.jsx
+++ resources/js/Components/NewsHeader.jsx
@@ -1,11 +1,8 @@
 export default function NewsHeader() {
-
     return (
-        <>
-            <div className="border-b">
-                <h1 className="text-left">A</h1>
-                <P className="text-right">Lihat semua</P>
-            </div>
-        </>
+        <div className="border-b">
+            <h1 className="text-left">A</h1>
+            <p className="text-right">Lihat semua</p>
+        </div>
     );
 }
--- routes/web.php
+++ routes/web.php
@@ -21,18 +21,18 @@ Route::get('/', [NewsController::class, 'index']);
 
 Route::get('/berita/{id}', [NewsController::class, 'berita']);
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/admin/dashboard', function () {
+//     return Inertia::render('Dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/welcome', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+// Route::get('/welcome', function () {
+//     return Inertia::render('Welcome', [
+//         'canLogin' => Route::has('login'),
+//         'canRegister' => Route::has('register'),
+//         'laravelVersion' => Application::VERSION,
+//         'phpVersion' => PHP_VERSION,
+//     ]);
+// });
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
