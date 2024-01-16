72371f2da1d17934aaf13f822f44d17533920abb
tasya <tasyaazzahraputri01@gmail.com>
2024-01-16 09:05:20 +0700

    News Header, Footer

--- resources/js/Components/NewsHeader.jsx
+++ resources/js/Components/NewsHeader.jsx
@@ -1,8 +1,16 @@
 export default function NewsHeader() {
     return (
-        <div className="border-b">
-            <h1 className="text-left">A</h1>
-            <p className="text-right">Lihat semua</p>
+        <div>
+            <button
+                type="button"
+                className="ml-4 px-4 bg-red-900 font-bold text-white btn-title p-2"
+            >
+                BERITA POPULER
+            </button>
+            <a href="" className="float-right px-4 text-dark text-sm mt-5 mb-0">
+                lihat semua
+            </a>
+            <hr className="ml-4 px-4 mb-2 border-red-900 border-t-2" />
         </div>
     );
 }
