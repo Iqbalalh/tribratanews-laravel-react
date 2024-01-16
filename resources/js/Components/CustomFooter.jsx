b361a2709a73819576d03d831f01621c0c97e7d1
tasya <tasyaazzahraputri01@gmail.com>
2024-01-16 09:05:20 +0700

    News Header, Footer

---
+++ resources/js/Components/CustomFooter.jsx
@@ -0,0 +1,109 @@
+import {
+    Footer,
+    FooterCopyright,
+    FooterIcon,
+    FooterLink,
+    FooterLinkGroup,
+} from "flowbite-react";
+import { IoLogoInstagram, IoLogoFacebook, IoLogoYoutube } from "react-icons/io";
+import { IoLogoTiktok } from "react-icons/io5";
+
+export default function CustomFooter() {
+    return (
+        <Footer className="flex flex-col justify-between min-h-screen">
+            <div className="flex items-end min-h-screen">
+                <img
+                    src="/assets/logo footer.png"
+                    className="w-full max-w-screen-md lg:max-w-screen-lg max-h-50"
+                />
+            </div>
+            <div className="px-4 mt-4 space-x-3 sm:flex sm:items-center sm:justify-end">
+                <FooterLinkGroup className="list-none mb-5 justify-center">
+                    <FooterLink
+                        className="text-lg font-bold text-blue-900 md:visible"
+                        href="#"
+                    >
+                        Polda Lampung
+                    </FooterLink>
+                    <FooterLink
+                        className="text-lg font-bold text-blue-900 md:visible"
+                        href="#"
+                    >
+                        Polres Lampung Utara
+                    </FooterLink>
+                    <FooterLink
+                        className="text-lg font-bold text-blue-900 md:visible"
+                        href="#"
+                    >
+                        Polres Lampung Tengah
+                    </FooterLink>
+                    <FooterLink
+                        className="text-lg font-bold text-blue-900 md:visible"
+                        href="#"
+                    >
+                        Polres Lampung Barat
+                    </FooterLink>
+                </FooterLinkGroup>
+            </div>
+            <div className="w-full bg-red-900 px-4 py-6 mt-2 mb-1 overflow-x-auto justify-end">
+                <div className="flex flex-col sm:flex-row justify-between">
+                    <FooterCopyright
+                        className="text-white text-sm md:visible lg:text-lg mb-4 sm:mb-0"
+                        href="#"
+                        by="Bidhumas Polda Lampung"
+                        year={2024}
+                    />
+                    <div className="flex flex-col mb-4 sm:mb-0 sm:flex-row sm:space-x list-none">
+                        <FooterLink
+                            className="text-white text-sm md:visible lg:text-lg"
+                            href="/tentang-kami"
+                        >
+                            Tentang Kami
+                        </FooterLink>
+                        <FooterLink
+                            className="text-white text-sm md:visible lg:text-lg"
+                            href="/redaksi"
+                        >
+                            Redaksi
+                        </FooterLink>
+                        <FooterLink
+                            className="text-white text-sm md:visible lg:text-lg"
+                            href="/pedoman-media-siber"
+                        >
+                            Pedoman Media Siber
+                        </FooterLink>
+                        <FooterLink
+                            className="text-white text-sm md:visible lg:text-lg"
+                            href="/kontak-kami"
+                        >
+                            Kontak Kami
+                        </FooterLink>
+                    </div>
+
+                    <div className="flex items-center space-x-5">
+                        <FooterIcon
+                            className="text-white"
+                            href="https://www.facebook.com/humaspoldalampung?mibextid=ZbWKwL"
+                            icon={IoLogoFacebook}
+                        />
+                        <FooterIcon
+                            className="text-white"
+                            href="https://www.instagram.com/humas_poldalampung?igsh=NnBtc21udXh0dDNz"
+                            icon={IoLogoInstagram}
+                        />
+                        <FooterIcon
+                            className="text-white"
+                            href="https://www.tiktok.com/@humaspoldalampung?_t=8iqtmZj54PY&_r=1"
+                            icon={IoLogoTiktok}
+                        />
+                        <FooterIcon
+                            className="text-white"
+                            href="https://youtube.com/@POLDALAMPUNG?si=MwqbMS8bQYIbgy-w"
+                            icon={IoLogoYoutube}
+                        />
+                    </div>
+                </div>
+            </div>
+        </Footer>
+    );
+}
--- resources/js/Pages/News.jsx
+++ resources/js/Pages/News.jsx
@@ -4,10 +4,14 @@ import NavBar from "@/Components/NavBar";
 import { Head } from "@inertiajs/react";
 import NewsHeader from "@/Components/NewsHeader";
 import NewsCardText from "@/Components/NewsCardText";
+import CustomFooter from "@/Components/CustomFooter";
 
 export default function (props) {
     return (
-        <div className="lg:flex lg:flex-col items-center justify-center" style={{ overflowX: 'hidden' }}>
+        <div
+            className="lg:flex lg:flex-col items-center justify-center"
+            style={{ overflowX: "hidden" }}
+        >
             <Head title="Home" />
             <div className="header lg:w-full lg:fixed top-0 z-50 bg-white justify-center">
                 <NavBar props={props} />
@@ -35,6 +39,9 @@ export default function (props) {
 
             <div className="hl-row lg:w-10/12 grid md:grid-cols-2 lg:grid-cols-3 mb-4">
                 <div className="lg:col-span-1 md:col-span-1">
+                    <div className="">
+                        <NewsHeader />
+                    </div>
                     <NewsCarousel props={props} />
                 </div>
                 <div className="lg:w-full lg:col-span-1 md:col-span-1">
@@ -56,7 +63,9 @@ export default function (props) {
                 </div>
             </div>
 
-            <div className="footer lg:w-10/12"></div>
+            <div className="footer lg:w-full">
+                <CustomFooter />
+            </div>
         </div>
     );
 }
